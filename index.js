3230942b2b4ac04ecd109e4c0dd6e74a80251e19
Romil <romil.m.kothari@gmail.com>
2023-05-01 18:00:04 -0400

    tests completed

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ document.addEventListener("DOMContentLoaded", () => {
             let percentChange = document.createElement("h4")
             percentChange.innerHTML = "Approx % change in 24 Hours:" + Math.round(element.changePercent24Hr) 
             div.appendChild(percentChange)
+            buildBullish(div);
        
         //   let bullish = document.createElement("p")
         //   bullish.innerText = `${element.likes} likes`
@@ -55,3 +56,22 @@ document.addEventListener("DOMContentLoaded", () => {
         //   div.appendChild(likeButton)
             document.getElementById("toy-collection").appendChild(div)
        } 
+
+
+       const EMPTY_HEART = '♡'
+       const FULL_HEART = '♥'
+       function buildBullish(div){
+            let bullish = document.createElement("h3")
+            bullish.innerHTML = "Bullish?" + EMPTY_HEART
+            bullish.addEventListener('click', function(e) {
+                if (e.target.innerText === "Bullish?" + EMPTY_HEART) {
+                    e.target.innerText = "Bullish Indeed!" + FULL_HEART;
+                    
+                  }
+                  else if (e.target.innerText === "Bullish Indeed!" + FULL_HEART) {
+                  e.target.innerText = "Bullish?" + EMPTY_HEART
+                  }
+            })
+
+            div.appendChild(bullish)
+       }
