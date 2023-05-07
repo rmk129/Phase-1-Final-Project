9238445235e99a750d16963d16eeb4b61e96dd55
Romil <romil.m.kothari@gmail.com>
2023-05-07 16:16:26 -0400

    added feature to comments that removed top comment after 4 commments are present

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
     })
     .then(e => {
         let dataArray = e.data.splice(0, 6)
-       dataArray.forEach(element => {
+        dataArray.forEach(element => {
             createCard(element)
        });
-         const button = document.getElementById("more-crypto-btn")
-         button.addEventListener('click', ()=>{
+        const button = document.getElementById("more-crypto-btn")
+        button.addEventListener('click', ()=>{
             let indexNumber = 6
             dataArray = e.data.splice(indexNumber, 6)
             dataArray.forEach(element => {
@@ -27,79 +27,89 @@ document.addEventListener("DOMContentLoaded", () => {
             newArray.forEach(element => {
                 createCard(element)
             })
-            indexNumber += 6
-            
-     }
+            indexNumber += 6            
+}
 
 
-
-       function createCard(element) {
-            let div = document.createElement("div");
-            div.className = "card";
-            let coinName = document.createElement("h2")
-            coinName.innerHTML = element.name;
-            div.appendChild(coinName);
-            let coinSymbol = document.createElement('h2')
-            coinSymbol.innerHTML = "Ticker/Symbol: " + element.symbol
-            div.appendChild(coinSymbol);
-            let price = document.createElement("h3")
-            price.innerHTML = "USD Price: " + parseFloat(element.priceUsd).toFixed(2)
-            div.appendChild(price)
-            let percentChange = document.createElement("h4")
-            percentChange.innerHTML = "Change in 24 Hours: " + Math.round(element.changePercent24Hr) + "%" 
-            div.appendChild(percentChange)
-            buildBullish(div);
-            buildComments(div);
-            buildMouseOver(div);
-            document.getElementById("crypto-collection").appendChild(div)
+function createCard(element) {
+        let div = document.createElement("div");
+        div.className = "card";
+        let coinName = document.createElement("h2")
+        coinName.innerHTML = element.name;
+        div.appendChild(coinName);
+        let coinSymbol = document.createElement('h2')
+        coinSymbol.innerHTML = "Ticker/Symbol: " + element.symbol
+        div.appendChild(coinSymbol);
+        let price = document.createElement("h3")
+        price.innerHTML = "USD Price: " + parseFloat(element.priceUsd).toFixed(2)
+        div.appendChild(price)
+        let percentChange = document.createElement("h4")
+        percentChange.innerHTML = "Change in 24 Hours: " + parseFloat(element.changePercent24Hr).toFixed(2) + "%" 
+        div.appendChild(percentChange)
+        buildBullish(div);
+        buildComments(div);
+        buildMouseOver(div);
+        document.getElementById("crypto-collection").appendChild(div)
        } 
 
 
        const EMPTY_HEART = '♡'
        const FULL_HEART = '♥'
        
-       function buildBullish(div){
-            let bullish = document.createElement("h3")
-            bullish.innerHTML = "Bullish?" + EMPTY_HEART
-            bullish.addEventListener('click', function(e) {
-                if (e.target.innerText === "Bullish?" + EMPTY_HEART) {
-                    e.target.innerText = "Bullish Indeed!" + FULL_HEART;
-                    
-                  }
-                  else if (e.target.innerText === "Bullish Indeed!" + FULL_HEART) {
-                  e.target.innerText = "Bullish?" + EMPTY_HEART
-                  }
+function buildBullish(div){
+        let bullish = document.createElement("h3")
+        bullish.innerHTML = "Bullish?" + EMPTY_HEART
+        bullish.addEventListener('click', function(e) {
+            if (e.target.innerText === "Bullish?" + EMPTY_HEART) {
+                e.target.innerText = "Bullish Indeed!" + FULL_HEART;
+                
+                }
+            else if (e.target.innerText === "Bullish Indeed!" + FULL_HEART) {
+                e.target.innerText = "Bullish?" + EMPTY_HEART
+                }
             })
-            div.appendChild(bullish)
+        div.appendChild(bullish)
        }
 
 function buildComments(div){
-    let form = document.createElement("form")
-    let label = document.createElement("label")
-    label.innerHTML = "Comments: "
-    form.appendChild(label)
-    let placeHolder = document.createElement("input")
-    placeHolder.type = "text"
-    placeHolder.id = "commentBox"
-    placeHolder.placeholder = "Price predictions?"
-    form.appendChild(placeHolder)
-    let submitBox = document.createElement("input")
-    submitBox.type = "submit"
-    submitBox.className = "submit"
-    submitBox.placeholder = "Comment Here..."
-    form.appendChild(submitBox)
-    let ul = document.createElement("ul")
-    form.appendChild(ul)
+        let form = document.createElement("form")
+        let label = document.createElement("label")
+        label.innerHTML = "Comments: "
+        form.appendChild(label)
+        let placeHolder = document.createElement("input")
+        placeHolder.type = "text"
+        placeHolder.id = "commentBox"
+        placeHolder.placeholder = "Price predictions?"
+        form.appendChild(placeHolder)
+        let submitBox = document.createElement("input")
+        submitBox.type = "submit"
+        submitBox.className = "submit"
+        submitBox.placeholder = "Comment Here..."
+        form.appendChild(submitBox)
+        let ul = document.createElement("ul")
+        ul.id = "cyptoComments"
+        form.appendChild(ul)
     
-    form.addEventListener('submit', (e) => {
-        e.preventDefault();
-        let li = document.createElement("li")
-        li.className = "comment-list"
-        li.innerHTML = e.target.commentBox.value
-        ul.appendChild(li)
-        form.reset()
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+            if (ul.childNodes.length > 3){
+            ul.childNodes[0].remove();
+            let li = document.createElement("li")
+            li.className = "comment-list"
+            li.innerHTML = e.target.commentBox.value
+            ul.appendChild(li)
+            form.reset()
+            }
+
+            else {
+                let li = document.createElement("li")
+            li.className = "comment-list"
+            li.innerHTML = e.target.commentBox.value
+            ul.appendChild(li)
+            form.reset()
+            }
     })
-    div.appendChild(form)
+        div.appendChild(form)
 
 }
 
@@ -107,12 +117,14 @@ function buildMouseOver(div) {
         div.addEventListener('mouseover', (e) => {
             div.style.width = "20rem"
             div.style.height = "30rem"
-            div.style.backgroundColor = "#7487f4"
+            div.style.boxShadow = "15px 20px #3b56f2"
+            div.style.backgroundColor = "#8092f79f"
             
         })
         div.addEventListener('mouseout', (e) => {
             div.style.width = "15rem"
             div.style.height = "25rem"
+            div.style.boxShadow = "8px 11px #3b56f2"
             div.style.backgroundColor = "white"
         })
 }
