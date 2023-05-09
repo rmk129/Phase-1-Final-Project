b31914989412df9921c61ae76700d8b6d20f3e27
Romil <romil.m.kothari@gmail.com>
2023-05-09 19:45:35 -0400

    cleaned up the code. made it more organized

--- index.js
+++ index.js
@@ -2,48 +2,43 @@ document.addEventListener("DOMContentLoaded", () => {
     fetch("https://api.coincap.io/v2/assets") 
     .then(function (resp) {
         return resp.json();
-    })
+})
     .then(e => {
         searchCrypto(e.data)
         resetCrypto(e.data)
         let dataArray = e.data.slice(0, 6)
         dataArray.forEach(element => {
             createCard(element)
-       });
+});
         let indexNumber1 = 6
         let indexNumber2 = 12
         const button = document.getElementById("more-crypto-btn")
         button.addEventListener('click', ()=>{
-            
-
             dataArray = e.data.slice(indexNumber1, indexNumber2)
             dataArray.forEach(element => {
                 createCard(element)
             })
             indexNumber1 += 6
             indexNumber2 += 6
-         })
-      }
-      )
-    })
+})})})
 
 
 
 function createCard(element) {
-        let div = document.createElement("div");
+        const div = document.createElement("div");
         div.className = "card";
-        let coinName = document.createElement("h2")
+        const coinName = document.createElement("h2")
         coinName.innerHTML = element.name;
         coinName.style.fontSize = "xx-large"
-        div.appendChild(coinName);
-        let coinSymbol = document.createElement('h2')
+        const coinSymbol = document.createElement('h2')
         coinSymbol.innerHTML = "Ticker/Symbol: " + element.symbol
-        div.appendChild(coinSymbol);
-        let price = document.createElement("h3")
+        const price = document.createElement("h3")
         price.innerHTML = "USD Price: " + parseFloat(element.priceUsd).toFixed(2)
-        div.appendChild(price)
-        let percentChange = document.createElement("h4")
+        const percentChange = document.createElement("h4")
         percentChange.innerHTML = "Change in 24 Hours: " + parseFloat(element.changePercent24Hr).toFixed(2) + "%" 
+        div.appendChild(coinName);
+        div.appendChild(coinSymbol);
+        div.appendChild(price)
         div.appendChild(percentChange)
 
         buildBullish(div);
@@ -57,37 +52,37 @@ function createCard(element) {
        const FULL_HEART = '♥'
        
 function buildBullish(div){
-        let bullish = document.createElement("h3")
+        const bullish = document.createElement("h3")
         bullish.innerHTML = "Bullish?" + EMPTY_HEART
         bullish.addEventListener('click', function(e) {
             if (e.target.innerText === "Bullish?" + EMPTY_HEART) {
                 e.target.innerText = "Bullish Indeed!" + FULL_HEART;
-                
                 }
             else if (e.target.innerText === "Bullish Indeed!" + FULL_HEART) {
                 e.target.innerText = "Bullish?" + EMPTY_HEART
-                }
-            })
+        } })
         div.appendChild(bullish)
-       }
+}
+
 
 function buildComments(div){
-        let form = document.createElement("form")
-        let label = document.createElement("label")
+        const form = document.createElement("form")
+        const label = document.createElement("label")
         label.innerHTML = "Comments: "
-        form.appendChild(label)
-        let placeHolder = document.createElement("input")
+        const placeHolder = document.createElement("input")
         placeHolder.type = "text"
         placeHolder.id = "commentBox"
         placeHolder.placeholder = "Price predictions?"
-        form.appendChild(placeHolder)
-        let submitBox = document.createElement("input")
+        const submitBox = document.createElement("input")
         submitBox.type = "submit"
         submitBox.className = "submit"
         submitBox.placeholder = "Comment Here..."
-        form.appendChild(submitBox)
-        let ul = document.createElement("ul")
+        const ul = document.createElement("ul")
         ul.id = "cyptoComments"
+
+        form.appendChild(label)
+        form.appendChild(placeHolder)
+        form.appendChild(submitBox)
         form.appendChild(ul)
     
         form.addEventListener('submit', (e) => {
@@ -97,54 +92,48 @@ function buildComments(div){
                 createLi(ul, e)
                 form.reset()
             }
-
             else {
                 createLi(ul, e)
                 form.reset()
-            }
-    })
+            } })
         div.appendChild(form)
 }
 
+function createLi(ul, e) {
+    const li = document.createElement("li")
+    li.className = "comment-list"
+    li.innerHTML = e.target.commentBox.value
+    ul.appendChild(li)
+}
+
 function searchCrypto (array){
-    let form = document.getElementById("searchCrypto")
+    const form = document.getElementById("searchCrypto")
     form.addEventListener('submit', (e) => {
         e.preventDefault();
-        let match = e.target.cryptoInput.value.toUpperCase();
+        const match = e.target.cryptoInput.value.toUpperCase();
         const newObj = array.find((element) => element.symbol === match) 
         
         if (newObj === undefined){
             alert("There is no matching crypto");
         }
         else {
-            let divCards = document.getElementById("crypto-collection")
+            const divCards = document.getElementById("crypto-collection")
             divCards.replaceChildren()
             createCard(newObj);
-            
-        }
-    })
+}})}
 
-}
 
 function resetCrypto(array) {
-    let reset = document.getElementById("reset-button")
-    console.log(reset)
+    const reset = document.getElementById("reset-button")
     reset.addEventListener('click', () => { 
-        let divCards = document.getElementById("crypto-collection")
+        const divCards = document.getElementById("crypto-collection")
         divCards.replaceChildren()
-        let dataArray = array.slice(0, 6)
+        const dataArray = array.slice(0, 6)
         dataArray.forEach(element => {
             createCard(element)
-        })  
-    })
-}
+})})}
 
-function createLi(ul, e) {
-    let li = document.createElement("li")
-    li.className = "comment-list"
-    li.innerHTML = e.target.commentBox.value
-    ul.appendChild(li)
-}
+
 
 function buildMouseOver(div) {
         div.addEventListener('mouseover', (e) => {
@@ -159,5 +148,4 @@ function buildMouseOver(div) {
             div.style.height = "25rem"
             div.style.boxShadow = "8px 11px #3b56f2"
             div.style.backgroundColor = "white"
-        })
-}
+})}
